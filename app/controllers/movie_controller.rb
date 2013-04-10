33675d693f0bef6fb372cd69b08afce16b71bd83
Livien Yin <LivienYin@gmail.com>
2013-04-10 13:03:16 -0700

    began styling home page and detail section

--- app/controllers/movie_controller.rb
+++ app/controllers/movie_controller.rb
@@ -5,9 +5,11 @@ class MovieController < ApplicationController
 
   def results
     @movie_search = MovieSearch.new params[:q]
+    # @movie_search =  MovieSearch.new.search params[:q]
   end
 
   def detail
+    @query = params[:q]
     @movie = Movie.build_from_id params[:id]
   end
 
--- app/models/movie_search.rb
+++ app/models/movie_search.rb
@@ -9,4 +9,11 @@ class MovieSearch
     result_entries = JSON.load(file.read)["Search"] || []
     @movies = result_entries.map{|movie_data| Movie.new movie_data}
   end
+
+  # separate from initialize method
+  # def search(query)
+  #   file = open("http://www.omdbapi.com/?s=#{URI.escape(query)}")
+  #   result_entries = JSON.load(file.read)["Search"] || []
+  #   @movies = result_entries.map{|movie_data| Movie.new movie_data}
+  # end
 end
